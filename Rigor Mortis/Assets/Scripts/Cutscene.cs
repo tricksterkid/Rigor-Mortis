68f1f610acceb0d9c00a14f1c5f86af43b241068
tricksterkid <deighton.zakelijk@gmail.com>
2020-06-30 10:07:01 +0200

    VERSION 9 - Rigor Mortis V2

--- Rigor Mortis/Assets/Scripts/Cutscene.cs
+++ Rigor Mortis/Assets/Scripts/Cutscene.cs
@@ -6,6 +6,7 @@ public class Cutscene : MonoBehaviour
     public GameObject cam1;
     public GameObject cam2;
     public GameObject ui;
+    public GameObject collider;
 
     void Start()
     {
@@ -15,8 +16,9 @@ public class Cutscene : MonoBehaviour
     IEnumerator Go()
     {
         yield return new WaitForSeconds(3);
+        cam2.SetActive(false);
+        collider.SetActive(false);
         cam1.SetActive(true);
         ui.SetActive(true);
-        cam2.SetActive(false);
     }
 }
---
+++ Rigor Mortis/Assets/Scripts/Pause.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public void PauseGame()
+    {
+        Time.timeScale = 0.0f;
+    }
+
+    public void PlayGame()
+    {
+        Time.timeScale = 1.0f;
+    }
+}
